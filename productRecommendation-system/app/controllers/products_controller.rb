8deaeec3316e8478d249085a61f6db51335d63c1
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-18 17:43:14 +0900

    2つのアプリケーションの連携完了

--- productRecommendation-system/app/controllers/products_controller.rb
+++ productRecommendation-system/app/controllers/products_controller.rb
@@ -1,12 +1,13 @@
+require 'net/http'
+
 class ProductsController < ApplicationController
-    def index
-      @products = Product.all
-      render json: @products
-    end
-  
-    def show
-      @product = Product.find(params[:id])
-      render json: @product
-    end
+  def index
+    @products = Product.all
+
+    uri = URI('http://localhost:5000/recommendations')
+    response = Net::HTTP.get(uri)
+    @recommendations = JSON.parse(response)
+
+    render json: { products: @products, recommendations: @recommendations }
   end
-  
+end
--- productRecommendation-system/config/routes.rb
+++ productRecommendation-system/config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :show]
-  # root 'products#index'
 end
