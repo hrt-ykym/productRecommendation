e30b1354232e89b3ca539db46684ec1c033b4a8a
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-01 18:19:15 +0900

    railsにもopentelemetryをいれたー

--- productRecommendation-system/config/application.rb
+++ productRecommendation-system/config/application.rb
@@ -11,6 +11,20 @@ module ProductRecommendationSystem
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # OpenTelemetryの設定
+    OpenTelemetry::SDK.configure do |c|
+      c.use_all() # 全てのインストルメンテーションを使用
+
+      c.service_name = 'rails-service'
+
+      c.add_span_processor(
+        OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
+          OpenTelemetry::Exporter::OTLP::Exporter.new(
+            endpoint: ENV['OTEL_EXPORTER_OTLP_ENDPOINT'] || 'http://tempo.observability.svc.cluster.local:4317'
+          )
+        )
+      )
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
