c9213046ed053a68149bfef3654edefdec89ee13
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-01 18:11:14 +0900

    openteremetry入れてみた

--- recommendationEngine/src/app.py
+++ recommendationEngine/src/app.py
@@ -1,30 +1,36 @@
 from flask import Flask, jsonify
-import logging
 import requests
 import random
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
+# トレーサーの設定
+resource = Resource(attributes={
+    "service.name": "flask-service"
+})
+
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://tempo.observability.svc.cluster.local:4317", insecure=True)
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
 
 class App:
     def __init__(self):
         self.app = Flask(__name__)
-
-        # ロガーの設定
-        logging.basicConfig(level=logging.DEBUG)
+        FlaskInstrumentor().instrument_app(self.app)
 
         @self.app.route('/recommendations', methods=['GET'])
         def get_recommendations():
-            try:
-                response = requests.get('http://rails-service.haruotsu.svc.cluster.local:80/products/api')
-                response.raise_for_status()
-                products = response.json()
-                recommended_products = random.sample(products, min(3, len(products)))
-                return jsonify(recommended_products)
-            except requests.exceptions.RequestException as e:
-                logging.error(f"Request failed: {e}")
-                return jsonify({"error": "Request to Rails service failed"}), 500
+            response = requests.get('http://rails-service.haruotsu.svc.cluster.local:80/products/api')
+            products = response.json()
+            recommended_products = random.sample(products, min(3, len(products)))
+            return jsonify(recommended_products)
 
     def run(self):
         self.app.run(host='0.0.0.0', port=5000)
-
-if __name__ == "__main__":
-    app = App()
-    app.run()
