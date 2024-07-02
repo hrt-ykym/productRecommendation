8902f8d40ed513ab74736d63559cdaa18e22867f
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-02 18:43:24 +0900

    require 'opentelemetry-exporter-otlp'

--- productRecommendation-system/config/initializers/opentelemetry.rb
+++ productRecommendation-system/config/initializers/opentelemetry.rb
@@ -1,6 +1,7 @@
 # config/initializers/opentelemetry.rb
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
+require 'opentelemetry-exporter-otlp'
 
 OpenTelemetry::SDK.configure do |c|
   c.service_name = ENV['OTEL_SERVICE_NAME'] || 'rails-app'
