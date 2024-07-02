2113e3305dd87836c18c787a0f67d17afd16fdc0
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-07-02 18:36:10 +0900

    rails-appがテレメトリーにでた

--- productRecommendation-system/config/initializers/opentelemetry.rb
+++ productRecommendation-system/config/initializers/opentelemetry.rb
@@ -1,6 +1,8 @@
+# config/initializers/opentelemetry.rb
 require 'opentelemetry/sdk'
 require 'opentelemetry/instrumentation/all'
+
 OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'rails-app'
+  c.service_name = ENV['OTEL_SERVICE_NAME'] || 'rails-app'
   c.use_all() # enables all instrumentation!
 end
